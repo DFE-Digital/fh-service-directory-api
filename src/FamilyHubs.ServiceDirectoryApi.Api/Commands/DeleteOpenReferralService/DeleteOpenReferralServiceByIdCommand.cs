61dba48a476835bd33f8e24b2ce806656eb828a8
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-09-14 16:02:53 +0100

    Added Delete Service

---
+++ src/FamilyHubs.ServiceDirectoryApi.Api/Commands/DeleteOpenReferralService/DeleteOpenReferralServiceByIdCommand.cs
@@ -0,0 +1,53 @@
+﻿using Ardalis.GuardClauses;
+using fh_service_directory_api.core.Entities;
+using fh_service_directory_api.infrastructure.Persistence.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace fh_service_directory_api.api.Commands.DeleteOpenReferralService;
+public class DeleteOpenReferralServiceByIdCommand : IRequest<bool>
+{
+    public DeleteOpenReferralServiceByIdCommand(string id)
+    {
+        Id = id;
+    }
+
+    public string Id { get; init; }
+}
+
+public class DeleteOpenReferralServiceByIdCommandHandler : IRequestHandler<DeleteOpenReferralServiceByIdCommand, bool>
+{
+    private readonly ApplicationDbContext _context;
+    private readonly ILogger<DeleteOpenReferralServiceByIdCommandHandler> _logger;
+
+    public DeleteOpenReferralServiceByIdCommandHandler(ApplicationDbContext context, ILogger<DeleteOpenReferralServiceByIdCommandHandler> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public async Task<bool> Handle(DeleteOpenReferralServiceByIdCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var entity = await _context.OpenReferralServices
+                .FirstOrDefaultAsync(p => p.Id == request.Id, cancellationToken: cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(OpenReferralService), request.Id);
+            }
+
+            entity.Status = "Deleted";
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred creating organisation. {exceptionMessage}", ex.Message);
+            throw new Exception(ex.Message, ex);
+        }
+    }
+}
--- tests/FamilyHubs.ServiceDirectoryApi.UnitTests/Services/WhenUsingServiceCommands .cs
+++ tests/FamilyHubs.ServiceDirectoryApi.UnitTests/Services/WhenUsingServiceCommands .cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using FamilyHubs.ServiceDirectoryApi.UnitTests.Organisation;
 using fh_service_directory_api.api.Commands.CreateOpenReferralOrganisation;
+using fh_service_directory_api.api.Commands.DeleteOpenReferralService;
 using fh_service_directory_api.api.Queries.GetServices;
 using fh_service_directory_api.core;
 using FluentAssertions;
@@ -37,5 +38,31 @@ public class WhenUsingServiceCommands : BaseCreateDbUnitTest
         ArgumentNullException.ThrowIfNull(testOrganisation, nameof(testOrganisation));
         ArgumentNullException.ThrowIfNull(testOrganisation.Services, nameof(testOrganisation.Services));
         results.Items[0].Should().BeEquivalentTo(testOrganisation.Services.ElementAt(0));
+    }
+
+    [Fact]
+    public async Task ThenDeleteOpenReferralService()
+    {
+        //Arange
+        var myProfile = new AutoMappingProfiles();
+        var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+        IMapper mapper = new Mapper(configuration);
+        var logger = new Mock<ILogger<CreateOpenReferralOrganisationCommandHandler>>();
+        var mockApplicationDbContext = GetApplicationDbContext();
+        var testOrganisation = WhenUsingOrganisationCommands.GetTestCountyCouncilDto();
+        CreateOpenReferralOrganisationCommand createcommand = new(testOrganisation);
+        CreateOpenReferralOrganisationCommandHandler createhandler = new(mockApplicationDbContext, mapper, logger.Object);
+        var id = await createhandler.Handle(createcommand, new System.Threading.CancellationToken());
+
+
+        DeleteOpenReferralServiceByIdCommand command = new("3010521b-6e0a-41b0-b610-200edbbeeb14");
+        DeleteOpenReferralServiceByIdCommandHandler handler = new(mockApplicationDbContext, new Mock<ILogger<DeleteOpenReferralServiceByIdCommandHandler>>().Object);
+
+        //Act
+        var results = await handler.Handle(command, new System.Threading.CancellationToken());
+
+        //Assert
+        results.Should().Be(true);
+        
     }
 }
