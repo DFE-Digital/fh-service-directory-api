5b55d8ca8e82f113c6a41ad6196f08631b8753a7
MartinBelton-gov <111495215+MartinBelton-gov@users.noreply.github.com>
2022-10-14 13:21:48 +0100

    Create and Update open referral service (#15)
    * Add and Update Service
    
    * Corrected Schedule Order
    
    * Added Holiday and Regular Schedules

---
+++ src/FamilyHubs.ServiceDirectoryApi.Api/Commands/CreateOpenReferralService/CreateOpenReferralServiceCommand.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralServices;
+using fh_service_directory_api.core.Entities;
+using fh_service_directory_api.core.Events;
+using fh_service_directory_api.infrastructure.Persistence.Repository;
+using MediatR;
+
+namespace fh_service_directory_api.api.Commands.CreateOpenReferralService;
+
+public class CreateOpenReferralServiceCommand : IRequest<string>
+{
+    public CreateOpenReferralServiceCommand(OpenReferralServiceDto openReferralService)
+    {
+        OpenReferralService = openReferralService;
+    }
+
+    public OpenReferralServiceDto OpenReferralService { get; init; }
+}
+
+public class CreateOpenReferralServiceCommandHandler : IRequestHandler<CreateOpenReferralServiceCommand, string>
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IMapper _mapper;
+    private readonly ILogger<CreateOpenReferralServiceCommandHandler> _logger;
+
+    public CreateOpenReferralServiceCommandHandler(ApplicationDbContext context, IMapper mapper, ILogger<CreateOpenReferralServiceCommandHandler> logger)
+    {
+        _context = context;
+        _mapper = mapper;
+        _logger = logger;
+    }
+
+    public async Task<string> Handle(CreateOpenReferralServiceCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var entity = _mapper.Map<OpenReferralService>(request.OpenReferralService);
+            ArgumentNullException.ThrowIfNull(entity, nameof(entity));
+
+            entity.RegisterDomainEvent(new OpenReferralServiceCreatedEvent(entity));
+
+            _context.OpenReferralServices.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred creating taxonomy. {exceptionMessage}", ex.Message);
+            throw new Exception(ex.Message, ex);
+        }
+
+        if (request is not null && request.OpenReferralService is not null)
+            return request.OpenReferralService.Id;
+        else
+            return string.Empty;
+    }
+}
--- tests/FamilyHubs.ServiceDirectoryApi.UnitTests/Organisation/WhenUsingOrganisationCommands.cs
+++ tests/FamilyHubs.ServiceDirectoryApi.UnitTests/Organisation/WhenUsingOrganisationCommands.cs
@@ -4,11 +4,13 @@ using FamilyHubs.ServiceDirectory.Shared.Enums;
 using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralContacts;
 using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralCostOptions;
 using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralEligibilitys;
+using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralHolidaySchedule;
 using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralLanguages;
 using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralLocations;
 using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralOrganisations;
 using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralPhones;
 using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralPhysicalAddresses;
+using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralRegularSchedule;
 using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralServiceAreas;
 using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralServiceAtLocations;
 using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralServiceDeliverysEx;
@@ -222,9 +224,9 @@ public class WhenUsingOrganisationCommands : BaseCreateDbUnitTest
                                     )
                             }
                             //new List<Accessibility_For_Disabilities>()
-                            )
-                        //new List<OpenReferralHoliday_Schedule>(),
-                        //new List<OpenReferralRegular_Schedule>()
+                            ),
+                        new List<OpenReferralRegularScheduleDto>(),
+                        new List<OpenReferralHolidayScheduleDto>()
                         )
 
                 })
@@ -360,8 +362,8 @@ public class WhenUsingOrganisationCommands : BaseCreateDbUnitTest
                             },
                             new List<Accessibility_For_Disabilities>()
                             ),
-                        new List<OpenReferralHoliday_Schedule>(),
-                        new List<OpenReferralRegular_Schedule>()
+                        new List<OpenReferralRegular_Schedule>(),
+                        new List<OpenReferralHoliday_Schedule>()
                         )
 
                 })
