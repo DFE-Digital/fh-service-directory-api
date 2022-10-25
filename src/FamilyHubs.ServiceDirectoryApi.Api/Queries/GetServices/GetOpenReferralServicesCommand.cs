48b5da83eb9c5ffd7f85f22792a4d3df538c66d9
AliShah2 <kishwer01@gmail.com>
2022-10-25 07:03:42 +0100

    Return services ordered by distance from request location

--- src/FamilyHubs.ServiceDirectoryApi.Api/Queries/GetServices/GetOpenReferralServicesCommand.cs
+++ src/FamilyHubs.ServiceDirectoryApi.Api/Queries/GetServices/GetOpenReferralServicesCommand.cs
@@ -74,7 +74,7 @@ public class GetOpenReferralServicesCommandHandler : IRequestHandler<GetOpenRefe
 
         IEnumerable<OpenReferralService> dbservices = entities;
         if (request?.Latitude != null && request?.Longtitude != null && request?.Meters != null)
-            dbservices = entities.Where(x => fh_service_directory_api.core.Helper.GetDistance(request.Latitude, request.Longtitude, x?.Service_at_locations?.FirstOrDefault()?.Location.Latitude, x?.Service_at_locations?.FirstOrDefault()?.Location.Longitude, x?.Name) < request.Meters);
+            dbservices = entities.Where(x => core.Helper.GetDistance(request.Latitude, request.Longtitude, x?.Service_at_locations?.FirstOrDefault()?.Location.Latitude, x?.Service_at_locations?.FirstOrDefault()?.Location.Longitude, x?.Name) < request.Meters);
 
         if (request?.MaximumAge != null)
             dbservices = dbservices.Where(x => x.Eligibilities.Any(x => x.Maximum_age <= request.MaximumAge.Value));
@@ -126,8 +126,16 @@ public class GetOpenReferralServicesCommandHandler : IRequestHandler<GetOpenRefe
             dbservices = new List<OpenReferralService>();
         }
 
+        if (request?.Latitude != null && request?.Longtitude != null && request?.Meters != null)
+            dbservices = dbservices.OrderBy(x => core.Helper.GetDistance(
+                request.Latitude,
+                request.Longtitude,
+                x?.Service_at_locations?.FirstOrDefault()?.Location.Latitude,
+                x?.Service_at_locations?.FirstOrDefault()?.Location.Longitude,
+                x?.Name));
+
         var filteredServices = OpenReferralDtoHelper.GetOpenReferralServicesDto(dbservices);
-        
+
         if (request != null)
         {
             var pagelist = filteredServices.Skip((request.PageNumber - 1) * request.PageSize).Take(request.PageSize).ToList();
