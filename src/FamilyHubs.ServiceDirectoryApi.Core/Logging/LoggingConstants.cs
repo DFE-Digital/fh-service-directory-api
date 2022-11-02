5f8457676100464d56c0b165948300faf5fd8bb7
vipin <115160396+vreddydfe@users.noreply.github.com>
2022-11-02 07:56:45 +0000

    Added Azure app insights to application logs  (#30)
    * Added Azure AppInsights Telemetry to the application logs
    
    * removed the appinsights key

---
+++ src/FamilyHubs.ServiceDirectoryApi.Core/Logging/LoggingConstants.cs
@@ -0,0 +1,11 @@
+﻿    /// <summary>
+    /// Logging Constants
+    /// </summary>
+    public static class LoggingConstants
+    {
+        /// <summary>
+        /// Property Name in Application Insights for currently running service
+        /// </summary>
+        public const string ServiceNamePropertiesName = "Service";
+    }
+
