cf2499e060467c4c2afcc1345a42792351910898
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-11-15 23:26:01 +0000

    Added ThenGetFamilyHubs

--- tests/FamilyHubs.ServiceDirectoryApi.UnitTests/FxSearch/WhenUsingFxSearchCommands.cs
+++ tests/FamilyHubs.ServiceDirectoryApi.UnitTests/FxSearch/WhenUsingFxSearchCommands.cs
@@ -1,16 +1,8 @@
 ﻿using AutoMapper;
-using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralLocations;
-using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralPhysicalAddresses;
-using FamilyHubs.ServiceDirectoryApi.UnitTests.Locations;
-using FamilyHubs.ServiceDirectoryApi.UnitTests.Organisation;
-using fh_service_directory_api.api.Commands.CreateModelLink;
-using fh_service_directory_api.api.Commands.CreateOpenReferralOrganisation;
 using fh_service_directory_api.api.Queries.FxSearch;
-using fh_service_directory_api.api.Queries.GetServices;
 using fh_service_directory_api.core;
 using fh_service_directory_api.infrastructure.Persistence.Repository;
 using FluentAssertions;
-using MediatR;
 using Microsoft.Extensions.Logging;
 using Moq;
 
@@ -25,43 +17,22 @@ public class WhenUsingFxSearchCommands : BaseCreateDbUnitTest
         var myProfile = new AutoMappingProfiles();
         var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
         IMapper mapper = new Mapper(configuration);
-        var logger = new Mock<ILogger<FxSearchCommandHandler>>();
+        var logger = new Mock<ILogger<ApplicationDbContextInitialiser>>();
         var mockApplicationDbContext = GetApplicationDbContext();
+        ApplicationDbContextInitialiser applicationDbContextInitialiser = new ApplicationDbContextInitialiser(logger.Object, mockApplicationDbContext);
+        await applicationDbContextInitialiser.SeedAsync();
 
-        var testLocation = await GetTestFamilyHub(mockApplicationDbContext, mapper);
 
-        FxSearchCommand searchCommand = new("XTEST", 1.0, 2.0);
+        FxSearchCommand searchCommand = new("E08000006", -2.3D, 53.6D);
         FxSearchCommandHandler searchCommandHandler = new(mockApplicationDbContext);
+        
 
         //Act
         var results = await searchCommandHandler.Handle(searchCommand, new CancellationToken());
 
         //Assert
         results.Should().NotBeNull();
-
-        results[0].First.Should().NotBeNull();
-        results[0].Second.Should().NotBeNull();
-
-        results.Count().Should().Be(2);
-        //results.Items[0].Should().BeEquivalentTo(testOrganisation.Services.ElementAt(0));
-    }
-
-    public async Task<OpenReferralLocationDto> GetTestFamilyHub(ApplicationDbContext mockApplicationDbContext, IMapper mapper)
-    {
-        var logger = new Mock<ILogger<CreateOpenReferralOrganisationCommandHandler>>();
-        var testOrganisation = WhenUsingOrganisationCommands.GetTestCountyCouncilDto();
-        CreateOpenReferralOrganisationCommand createOrgCommand = new(testOrganisation);
-        CreateOpenReferralOrganisationCommandHandler orghandler = new(mockApplicationDbContext, mapper, logger.Object);
-        
-        var orgresult = await orghandler.Handle(createOrgCommand, new System.Threading.CancellationToken());
-
-        var mockMediator = new Mock<ISender>();
-        mockMediator.Setup(x => x.Send(It.IsAny<CreateModelLinkCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync("All Done");
-
-        var addresses = new List<OpenReferralPhysicalAddressDto>() { new OpenReferralPhysicalAddressDto("e823d7ef-a9c4-4782-ad52-91e642ebb895", "Test Street", "Manchester", "M7 7BQ", "United Kingdom", "Salford") };
-        OpenReferralLocationDto location = new OpenReferralLocationDto("0c1111fd-7817-49ae-b599-4d15e504fe8b", "Test Family Hub", "Test Hub", -2.359764D, 53.407025D, addresses);
-
-        return location;
+        results.Count().Should().BeGreaterThan(2);
     }
 }
 
