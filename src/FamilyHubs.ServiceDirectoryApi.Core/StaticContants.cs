463f8f53808f5a278d44c4091d18ecc1ca99a8a6
MartinBelton-gov <111495215+MartinBelton-gov@users.noreply.github.com>
2022-11-28 15:33:15 +0000

    Modified Seed Data for Referral UI (#57)

--- src/FamilyHubs.ServiceDirectoryApi.Core/StaticContants.cs
+++ src/FamilyHubs.ServiceDirectoryApi.Core/StaticContants.cs
@@ -2,6 +2,6 @@
 
 public static class StaticContants
 {
-    public const string Location_Taxonomy = "Location-Taxonomy";
-    public const string Location_Organisation = "Location-Organisation";
+    public const string Location_Taxonomy = "Location_Taxonomy";
+    public const string Location_Organisation = "Location_Organisation";
 }
