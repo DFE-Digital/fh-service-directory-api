223d741a5614b48f256b63d43dc492faa9b2309f
skumarhpa <skumar.hpa@gmail.com>
2023-01-31 13:48:00 +0000

    removed unused code

--- src/FamilyHubs.ServiceDirectoryApi.Api/Commands/UpdateOpenReferralService/UpdateOpenReferralServiceCommand.cs
+++ src/FamilyHubs.ServiceDirectoryApi.Api/Commands/UpdateOpenReferralService/UpdateOpenReferralServiceCommand.cs
@@ -467,14 +467,7 @@ public class UpdateOpenReferralServiceCommandHandler : IRequestHandler<UpdateOpe
             }
             else
             {
-            //    if (current.Taxonomy != null && updatedServiceTaxonomy.Taxonomy != null)
-            //    {
-            //        current.Taxonomy.Name = updatedServiceTaxonomy.Taxonomy.Name;
-            //        current.Taxonomy.Vocabulary = updatedServiceTaxonomy.Taxonomy.Vocabulary;
-            //        current.Taxonomy.Parent = updatedServiceTaxonomy.Taxonomy.Parent;
-            //    }
-
-                currentIds.Add(current?.Id);
+              currentIds.Add(current?.Id);
             }
         }
 
