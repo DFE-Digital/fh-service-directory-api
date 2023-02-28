c012bdc99e961dc283aa4567d33a8efb4e1f8960
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-02-28 09:00:43 +0000

    Prevent malfored URL's being entered

--- src/FamilyHubs.ServiceDirectoryApi.Api/Commands/UpdateService/UpdateServiceCommandValidator.cs
+++ src/FamilyHubs.ServiceDirectoryApi.Api/Commands/UpdateService/UpdateServiceCommandValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace FamilyHubs.ServiceDirectory.Api.Commands.UpdateService;
 
@@ -26,5 +27,44 @@ public class UpdateServiceCommandValidator : AbstractValidator<UpdateServiceComm
             .MaximumLength(50)
             .NotNull()
             .NotEmpty();
+
+        RuleFor(v => v.Service.LinkContacts).Custom((list, context) =>
+        {
+            if (list != null)
+            {
+                var hasInvalidUrl = list.Any(x => x != null && x.Contact != null && x.Contact.Url != null && !IsValidURL(x.Contact.Url));
+                if (hasInvalidUrl)
+                {
+                    context.AddFailure("Contact Url must be valid");
+                }
+            }
+        });
+
+        RuleFor(v => v.Service.ServiceAtLocations).Custom((list, context) =>
+        {
+            if (list != null)
+            {
+                foreach (var item in list.Select(x => x.LinkContacts))
+                {
+                    if (item != null)
+                    {
+                        var hasInvalidUrl = item.Any(x => x != null && x.Contact != null && x.Contact.Url != null && !IsValidURL(x.Contact.Url));
+                        if (hasInvalidUrl)
+                        {
+                            context.AddFailure("Contact Url must be valid");
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private static bool IsValidURL(string URL)
+    {
+        if (string.IsNullOrEmpty(URL))
+            return true;
+        string Pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
+        Regex Rgx = new(Pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        return Rgx.IsMatch(URL);
     }
 }
--- tests/FamilyHubs.ServiceDirectoryApi.UnitTests/Services/WhenValidatingServiceCommands.cs
+++ tests/FamilyHubs.ServiceDirectoryApi.UnitTests/Services/WhenValidatingServiceCommands.cs
@@ -65,4 +65,136 @@ public class WhenValidatingServiceCommands
         //Assert
         result.Errors.Any().Should().BeFalse();
     }
+
+    [Theory]
+    [InlineData(default!)]
+    [InlineData("")]
+    [InlineData("http://wwww.google.co.uk")]
+    [InlineData("http://wwww.google.com")]
+    [InlineData("https://wwww.google.com")]
+    public void ThenShouldValidateServiceAtLocationContactUrlWhenCreatingService_ShouldReturnNoErrors(string url)
+    {
+        //Arrange
+        var testService = TestDataProvider.GetTestCountyCouncilServicesDto2("56e62852-1b0b-40e5-ac97-54a67ea957dc");
+        if (testService.ServiceAtLocations is not null)
+        {
+            foreach (var serviceAtLocation in testService.ServiceAtLocations)
+            {
+                if (serviceAtLocation is not null && serviceAtLocation.LinkContacts is not null)
+                {
+                    foreach (var item in serviceAtLocation.LinkContacts)
+                    {
+                        item.Contact.Url = url;
+                    }
+                }
+            }
+        }
+        
+
+        var validator = new CreateServiceCommandValidator();
+        var testModel = new CreateServiceCommand(testService);
+
+        //Act
+        var result = validator.Validate(testModel);
+
+        //Assert
+        result.Errors.Any().Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData("someurl")]
+    [InlineData("http://someurl")]
+    [InlineData("https://someurl")]
+    public void ThenShouldValidateServiceAtLocationContactUrlWhenCreatingService_ShouldReturnErrors(string url)
+    {
+        //Arrange
+        var testService = TestDataProvider.GetTestCountyCouncilServicesDto2("56e62852-1b0b-40e5-ac97-54a67ea957dc");
+        if (testService.ServiceAtLocations is not null)
+        {
+            foreach (var serviceAtLocation in testService.ServiceAtLocations)
+            {
+                if (serviceAtLocation is not null && serviceAtLocation.LinkContacts is not null)
+                {
+                    foreach (var item in serviceAtLocation.LinkContacts)
+                    {
+                        item.Contact.Url = url;
+                    }
+                }
+            }
+        }
+
+
+        var validator = new CreateServiceCommandValidator();
+        var testModel = new CreateServiceCommand(testService);
+
+        //Act
+        var result = validator.Validate(testModel);
+
+        //Assert
+        result.Errors.Any().Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(default!)]
+    [InlineData("")]
+    [InlineData("http://wwww.google.co.uk")]
+    [InlineData("http://wwww.google.com")]
+    [InlineData("https://wwww.google.com")]
+    public void ThenShouldValidateServiceAtLocationContactUrlWhenUpdatingService_ShouldReturnNoErrors(string url)
+    {
+        //Arrange
+        var testService = TestDataProvider.GetTestCountyCouncilServicesDto2("56e62852-1b0b-40e5-ac97-54a67ea957dc");
+        if (testService.ServiceAtLocations is not null)
+        {
+            foreach (var serviceAtLocation in testService.ServiceAtLocations)
+            {
+                if (serviceAtLocation is not null && serviceAtLocation.LinkContacts is not null)
+                {
+                    foreach (var item in serviceAtLocation.LinkContacts)
+                    {
+                        item.Contact.Url = url;
+                    }
+                }
+            }
+        }
+        var validator = new UpdateServiceCommandValidator();
+        var testModel = new UpdateServiceCommand(testService.Id, testService);
+
+        //Act
+        var result = validator.Validate(testModel);
+
+        //Assert
+        result.Errors.Any().Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData("someurl")]
+    [InlineData("http://someurl")]
+    [InlineData("https://someurl")]
+    public void ThenShouldValidateServiceAtLocationContactUrlWhenUpdatingService_ShouldReturnErrors(string url)
+    {
+        //Arrange
+        var testService = TestDataProvider.GetTestCountyCouncilServicesDto2("56e62852-1b0b-40e5-ac97-54a67ea957dc");
+        if (testService.ServiceAtLocations is not null)
+        {
+            foreach (var serviceAtLocation in testService.ServiceAtLocations)
+            {
+                if (serviceAtLocation is not null && serviceAtLocation.LinkContacts is not null)
+                {
+                    foreach (var item in serviceAtLocation.LinkContacts)
+                    {
+                        item.Contact.Url = url;
+                    }
+                }
+            }
+        }
+        var validator = new UpdateServiceCommandValidator();
+        var testModel = new UpdateServiceCommand(testService.Id, testService);
+
+        //Act
+        var result = validator.Validate(testModel);
+
+        //Assert
+        result.Errors.Any().Should().BeTrue();
+    }
 }
