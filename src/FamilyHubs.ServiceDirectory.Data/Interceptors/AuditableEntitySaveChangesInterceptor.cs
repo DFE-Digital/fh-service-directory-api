49a031de49b789dc3e0e95c09d8b7292f57a70e9
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-06-27 13:32:09 +0100

    Createdby reflects user

--- src/FamilyHubs.ServiceDirectory.Data/Interceptors/AuditableEntitySaveChangesInterceptor.cs
+++ src/FamilyHubs.ServiceDirectory.Data/Interceptors/AuditableEntitySaveChangesInterceptor.cs
@@ -1,12 +1,21 @@
 ﻿using FamilyHubs.ServiceDirectory.Data.Entities.Base;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using FamilyHubs.SharedKernel.Identity;
 
 namespace FamilyHubs.ServiceDirectory.Data.Interceptors;
 
 public class AuditableEntitySaveChangesInterceptor : SaveChangesInterceptor
 {
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public AuditableEntitySaveChangesInterceptor(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
     public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
     {
         UpdateEntities(eventData.Context);
@@ -25,6 +34,13 @@ public class AuditableEntitySaveChangesInterceptor : SaveChangesInterceptor
     {
         if (context is null) return;
 
+        var updatedBy = "System";
+        var user = _httpContextAccessor?.HttpContext?.GetFamilyHubsUser();
+        if (user != null && !string.IsNullOrEmpty(user.Email))
+        {
+            updatedBy = user.Email;
+        }
+
         foreach (var entry in context.ChangeTracker.Entries<ServiceLocationSharedEntityBase>())
         {
             if (entry is { State: EntityState.Modified, Entity: { ServiceId: null, LocationId: null } })
@@ -37,13 +53,13 @@ public class AuditableEntitySaveChangesInterceptor : SaveChangesInterceptor
         {
             if (entry.State == EntityState.Added)
             {
-                entry.Entity.CreatedBy = "System";
+                entry.Entity.CreatedBy = updatedBy;
                 entry.Entity.Created = DateTime.UtcNow;
             }
 
             if (entry.State is EntityState.Added or EntityState.Modified || entry.HasChangedOwnedEntities())
             {
-                entry.Entity.LastModifiedBy = "System";
+                entry.Entity.LastModifiedBy = updatedBy;
                 entry.Entity.LastModified = DateTime.UtcNow;
             }
         }
