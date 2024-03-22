73095d6c138f34129180ac98797ce2280deeaba0
Phil Davies <github@phildavies.co.uk>
2024-03-22 16:07:31 +0000

    remove now uneeded code

--- src/FamilyHubs.ServiceDirectory.Core/Helper/HelperUtility.cs
+++ src/FamilyHubs.ServiceDirectory.Core/Helper/HelperUtility.cs
@@ -1,12 +1,9 @@
 ﻿using Ardalis.GuardClauses;
-using AutoMapper;
 using FamilyHubs.ServiceDirectory.Data.Entities;
 using FamilyHubs.ServiceDirectory.Data.Entities.Base;
-using FamilyHubs.ServiceDirectory.Data.Repository;
 using GeoCoordinatePortable;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
-using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 
 namespace FamilyHubs.ServiceDirectory.Core.Helper;
@@ -18,20 +15,6 @@ public static class HelperUtility
     private static readonly Regex IsValidUrlRegEx = new(
         @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$",
         RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
-    //public static async Task<TEntity> GetEntity<TEntity>(
-    //    this long id,
-    //    DbSet<TEntity> existingEntities)
-    //    where TEntity : EntityBase<long>
-    //{
-    //    var foundEntity = await existingEntities.FindAsync(id);
-    //    if (foundEntity == null)
-    //    {
-    //        throw new NotFoundException(id.ToString(), nameof(TEntity));
-    //    }
-
-    //    return foundEntity;
-    //}
 
     public static async Task<List<TEntity>> GetEntities<TEntity>(
         this ICollection<long> ids,
@@ -56,87 +39,12 @@ public static class HelperUtility
         return foundEntities;
     }
 
-    //todo: generic for entity
-    public static async Task<List<TEntity>> LinkExistingEntities<TEntity>(
-        this IList<TEntity> entities,
-        DbSet<TEntity> existingEntities,
-        IMapper mapper,
-        bool mapExistingEntity = true)
-        where TEntity : EntityBase<long>
-    {
-        List<TEntity> linkedEntities = new();
-        foreach (TEntity entity in entities)
-        {
-            TEntity newEntity;
-            //or IsKeySet
-            if (entity.Id != 0)
-            {
-                TEntity existingLocation = await existingEntities.FindAsync(entity.Id)
-                    ?? throw new NotFoundException(nameof(TEntity), entity.Id.ToString());
-
-                if (mapExistingEntity)
-                {
-                    mapper.Map(entity, existingLocation);
-                }
-
-                newEntity = existingLocation;
-            }
-            else
-            {
-                newEntity = entity;
-            }
-
-            linkedEntities.Add(newEntity);
-        }
-
-        return linkedEntities;
-    }
-
-    public static void AttachExistingManyToMany(this Service service, ApplicationDbContext context, IMapper mapper)
-    {
-        var existingTaxonomies = service.Taxonomies.Select(s => s.Name).ToList();
-        service.Taxonomies = service.Taxonomies.AddOrAttachExisting(context, mapper,
-            t => existingTaxonomies.Contains(t.Name),
-            (s, d) => s.Name == d.Name);
-    }
-
-    public static IList<TEntity> AddOrAttachExisting<TEntity>(this IList<TEntity> unSavedEntities,
-        ApplicationDbContext context,
-        IMapper mapper,
-        Expression<Func<TEntity, bool>> searchPredicate,
-        Func<TEntity, TEntity, bool> matchingPredicate
-    ) where TEntity : EntityBase<long>
-    {
-        var returnList = new List<TEntity>();
-
-        if (!unSavedEntities.Any())
-            return returnList;
-
-        var existing = context.Set<TEntity>().Where(searchPredicate).ToList();
-
-        foreach (var unSavedItem in unSavedEntities)
-        {
-            var savedItem = existing.SingleOrDefault(saved => matchingPredicate(saved, unSavedItem));
-
-            if (savedItem is null)
-            {
-                returnList.Add(unSavedItem);
-            }
-            else
-            {
-                unSavedItem.Id = savedItem.Id;
-                savedItem = mapper.Map(unSavedItem, savedItem);
-                returnList.Add(savedItem);
-            }
-        }
-
-        return returnList;
-    }
-
     public static bool IsValidUrl(string url)
     {
         return IsValidUrlRegEx.IsMatch(url);
     }
+
+    //todo: throw this away and do it in the db using EF, see https://learn.microsoft.com/en-us/ef/core/modeling/spatial
 
     //return distance in meters https://stackoverflow.com/questions/6366408/calculating-distance-between-two-latitude-and-longitude-geocoordinates
     public static double GetDistance(double? latitude1, double? longitude1, double? latitude2, double? longitude2, string? name = null)
