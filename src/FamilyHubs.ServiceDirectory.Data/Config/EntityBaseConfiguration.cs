2bade8f4bfab574e1583171f8f5848ab718c79c7
Phil Davies <github@phildavies.co.uk>
2024-05-24 10:49:46 +0100

    add EntityBaseConfiguration

---
+++ src/FamilyHubs.ServiceDirectory.Data/Config/EntityBaseConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using FamilyHubs.ServiceDirectory.Data.Entities.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.DataEncryption;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FamilyHubs.ServiceDirectory.Data.Config;
+
+public class EntityBaseConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+    where TEntity : EntityBase<long>
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.Property(t => t.Created)
+            .IsRequired();
+
+        builder.Property(t => t.CreatedBy)
+            .HasMaxLength(MaxLength.Email)
+            .IsRequired()
+            .IsEncrypted();
+
+        builder.Property(t => t.LastModifiedBy)
+            .HasMaxLength(MaxLength.Email)
+            .IsEncrypted();
+    }
+}
