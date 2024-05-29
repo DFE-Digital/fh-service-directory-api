4d38af6b369e5243c3b1ff0584f7049163d2beab
DamiLaleye1 <133227959+DamiLaleye1@users.noreply.github.com>
2024-05-29 15:43:59 +0100

    Added test scenarios

--- tests/FamilyHubs.ServiceDirectory.Api.AcceptanceTests/Tests/Stories/ServiceSearchTests.cs
+++ tests/FamilyHubs.ServiceDirectory.Api.AcceptanceTests/Tests/Stories/ServiceSearchTests.cs
@@ -6,28 +6,91 @@ namespace FamilyHubs.ServiceDirectory.Api.AcceptanceTests.Tests.Stories;
 
 // Define the story/feature being tested
 [Story(
-    AsA = "user of the metrics api",
-    IWant = "to be able to update usage information for postcode searches and filters",
-    SoThat = "I can record how search functionality is being used")]
+   AsA = "user of the metrics api",
+   IWant = "to be able to update usage information for postcode searches and filters",
+   SoThat = "I can record how search functionality is being used")]
 
 [TestClass]
 public class ServiceSearchTests
 {
-    private readonly ServiceSearchSteps steps;
+   private readonly ServiceSearchSteps steps;
 
-    public ServiceSearchTests()
-    {
-        //Get instances of the steps required for the test
-        steps = new ServiceSearchSteps();
-    }
+   public ServiceSearchTests()
+   {
+       //Get instances of the steps required for the test
+       steps = new ServiceSearchSteps();
+   }
 
-    //Add all tests that make up the story to this class.
-    [TestMethod]
-    public void Users_Can_Update_Metrics_For_Postcode_Search()
-    {
-        this.Given(s => steps.GivenIHaveAPostcodeToSearchWithinFind())
-            .When(s => steps.WhenISendARequest())
-            .Then(s => steps.Then200StatusCodeReturned())
-            .BDDfy();
-    }
+   //Add all tests that make up the story to this class.
+   [TestMethod]
+   public void Initial_Postcode_Search_as_a_Find_User()
+   {
+       this.Given(s => steps.GivenIPostToTheServiceSearchEndpointWithParameters("15","E1 2EN","200","1","2"))
+           .When(s => steps.WhenISendARequest())
+           .Then(s => steps.Then200StatusCodeReturned())
+           .BDDfy();
+   }
+  
+   [TestMethod]
+   public void Initial_Postcode_Search_as_a_Connect_User()
+   {
+       this.Given(s => steps.GivenIPostToTheServiceSearchEndpointWithParameters("15","E1 2EN","200","1","1"))
+           .When(s => steps.WhenISendARequest())
+           .Then(s => steps.Then200StatusCodeReturned())
+           .BDDfy();
+   }
+   [TestMethod]
+   public void Subsequent_Filter_Postcode_Search_as_a_Find_User()
+   {
+       this.Given(s => steps.GivenIPostToTheServiceSearchEndpointWithParameters("15","E1 2EN","200","2","2"))
+           .When(s => steps.WhenISendARequest())
+           .Then(s => steps.Then200StatusCodeReturned())
+           .BDDfy();
+   }
+   [TestMethod]
+   public void Subsequent_Filter_Postcode_Search_as_a_Connect_User()
+   {
+       this.Given(s => steps.GivenIPostToTheServiceSearchEndpointWithParameters("15","E1 2EN","200","2","1"))
+           .When(s => steps.WhenISendARequest())
+           .Then(s => steps.Then200StatusCodeReturned())
+           .BDDfy();
+   }
+  
+  
+   //Negative Scenarios
+   [TestMethod]
+   public void No_metrics_are_sent_when_a_non_200_message_is_received_from_postcode_IO()
+   {
+       this.Given(s => steps.GivenIPostToTheServiceSearchEndpointWithParameters("15","E1 2EN","400","1","1"))
+           .When(s => steps.WhenISendARequest())
+           .Then(s => steps.Then500StatusCodeReturned())
+           .BDDfy();
+   }
+
+   [TestMethod]
+   public void ErrorCode_when_wrong_searchTriggerEventType_is_sent()
+   {
+       this.Given(s => steps.GivenIPostToTheServiceSearchEndpointWithParameters("15","E1 2EN","200","3","1"))
+           .When(s => steps.WhenISendARequest())
+           .Then(s => steps.Then500StatusCodeReturned())
+           .BDDfy();
+   }
+
+   [TestMethod]
+   public void ErrorCode_message_when_wrong_serviceSearchTypeId_is_posted()
+   {
+       this.Given(s => steps.GivenIPostToTheServiceSearchEndpointWithParameters("15","E1 2EN","200","1","3"))
+           .When(s => steps.WhenISendARequest())
+           .Then(s => steps.Then500StatusCodeReturned())
+           .BDDfy();
+   }
+  
+   [TestMethod]
+   public void ErrorCode_message_when_wrong_searchRadiusMiles_is_posted()
+   {
+       this.Given(s => steps.GivenIPostToTheServiceSearchEndpointWithParameters("500","E1 2EN","200","1","1"))
+           .When(s => steps.WhenISendARequest())
+           .Then(s => steps.Then400StatusCodeReturned())
+           .BDDfy();
+   }
 }
