5bed3e3e3a6f4816b88d5c61c928525acedf52b0
Tina Gohil <tinagohil@Tinas-MBP.localdomain>
2024-06-28 16:17:18 +0100

    refactor: combined tests together

--- acceptance-tests/FamilyHubs.ServiceDirectory.Api.AcceptanceTests/Tests/Stories/ServiceSearchTests.cs
+++ acceptance-tests/FamilyHubs.ServiceDirectory.Api.AcceptanceTests/Tests/Stories/ServiceSearchTests.cs
@@ -23,8 +23,11 @@ public class ServiceSearchTests
 
     //Add all tests that make up the story to this class.
     [Theory]
-    [InlineData("15", "E1 2EN", "200", "1", "2", HttpStatusCode.OK)]
-    public void Service_Search_Metrics_Endpoint_Returns_A_200_When_Initial_Postcode_Search_Is_Done_As_A_Find_User(
+    [InlineData("20", "E1 2EN", "200", "1", "2", HttpStatusCode.OK)] // As a Find user
+    [InlineData("5", "E1 2EN", "401", "2", "2", HttpStatusCode.OK)] // As a Find user subsequent search
+    [InlineData("20", "E1 2EN", "500", "1", "1", HttpStatusCode.OK)] // As a Connect user
+    [InlineData("0", "E1 2EN", "400", "2", "1", HttpStatusCode.OK)] // As a Connect user subsequent search
+    public void Service_Search_Metrics_Endpoint_Returns_A_Status_Code_Ok(
         string radius, string postcode, string statusCode, string searchTriggerEventId, string serviceSearchTypeId,
         HttpStatusCode expectedStatusCode)
     {
@@ -35,143 +38,15 @@ public class ServiceSearchTests
             .Then(s => _steps.ThenExpectedStatusCodeReturned(expectedStatusCode))
             .BDDfy();
     }
+    
 
-    [Theory]
-    [InlineData("15", "E1 2EN", "200", "1", "1", HttpStatusCode.OK)]
-    public void
-        Service_Search_Metrics_Endpoint_Returns_A_200_When_An_Initial_Postcode_Search_Is_Done_As_A_Connect_User(
-            string radius, string postcode, string statusCode, string searchTriggerEventId, string serviceSearchTypeId,
-            HttpStatusCode expectedStatusCode)
-    {
-        this.Given(s =>
-                _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId,
-                    serviceSearchTypeId))
-            .When(s => _steps.WhenISendARequest())
-            .Then(s => _steps.ThenExpectedStatusCodeReturned(expectedStatusCode))
-            .BDDfy();
-    }
-
-    [Theory]
-    [InlineData("15", "E1 2EN", "200", "2", "2", HttpStatusCode.OK)]
-    public void
-        Service_Search_Metrics_Endpoint_Returns_A_200_When_A_Subsequent_Filter_Postcode_Search_Is_Done_As_A_Find_User(
-            string radius, string postcode, string statusCode, string searchTriggerEventId, string serviceSearchTypeId,
-            HttpStatusCode expectedStatusCode)
-    {
-        this.Given(s =>
-                _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId,
-                    serviceSearchTypeId))
-            .When(s => _steps.WhenISendARequest())
-            .Then(s => _steps.ThenExpectedStatusCodeReturned(expectedStatusCode))
-            .BDDfy();
-    }
-
-    [Theory]
-    [InlineData("15", "E1 2EN", "200", "2", "1", HttpStatusCode.OK)]
-    public void
-        Service_Search_Metrics_Endpoint_Returns_A_200_When_A_Subsequent_Filter_Postcode_Search_Is_Done_As_A_Connect_User(
-            string radius, string postcode, string statusCode, string searchTriggerEventId, string serviceSearchTypeId,
-            HttpStatusCode expectedStatusCode)
-    {
-        this.Given(s =>
-                _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId,
-                    serviceSearchTypeId))
-            .When(s => _steps.WhenISendARequest())
-            .Then(s => _steps.ThenExpectedStatusCodeReturned(expectedStatusCode))
-            .BDDfy();
-    }
-
-    [Theory]
-    [InlineData("15", "E1 2EN", "300", "1", "2", HttpStatusCode.OK)]
-    public void
-        Service_Search_Metrics_Endpoint_Returns_A_200_For_An_Initial_Postcode_Search_As_A_Find_User_After_the_Service_Search_API_Responds_With_A_Non_200_Response_Code(
-            string radius, string postcode, string statusCode, string searchTriggerEventId, string serviceSearchTypeId,
-            HttpStatusCode expectedStatusCode)
-    {
-        this.Given(s =>
-                _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId,
-                    serviceSearchTypeId))
-            .When(s => _steps.WhenISendARequest())
-            .Then(s => _steps.ThenExpectedStatusCodeReturned(expectedStatusCode))
-            .BDDfy();
-    }
-
-    [Theory]
-    [InlineData("15", "E1 2EN", "500", "1", "1", HttpStatusCode.OK)]
-    public void
-        Service_Search_Metrics_Endpoint_Returns_A_200_For_An_Initial_Postcode_Search_As_A_Connect_User_After_Service_Search_API_Responds_With_A_Non_200_Response_Code(
-            string radius, string postcode, string statusCode, string searchTriggerEventId, string serviceSearchTypeId,
-            HttpStatusCode expectedStatusCode)
-    {
-        this.Given(s =>
-                _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId,
-                    serviceSearchTypeId))
-            .When(s => _steps.WhenISendARequest())
-            .Then(s => _steps.ThenExpectedStatusCodeReturned(expectedStatusCode))
-            .BDDfy();
-    }
-
-    [Theory]
-    [InlineData("15", "E1 2EN", "422", "2", "2", HttpStatusCode.OK)]
-    public void
-        Service_Search_Metrics_Endpoint_Returns_A_200_For_A_Subsequent_Postcode_Search_As_A_Find_User_After_the_Service_Search_API_Responds_With_A_Non_200_Response_Code(
-            string radius, string postcode, string statusCode, string searchTriggerEventId, string serviceSearchTypeId,
-            HttpStatusCode expectedStatusCode)
-    {
-        this.Given(s =>
-                _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId,
-                    serviceSearchTypeId))
-            .When(s => _steps.WhenISendARequest())
-            .Then(s => _steps.ThenExpectedStatusCodeReturned(expectedStatusCode))
-            .BDDfy();
-    }
-
-    [Theory]
-    [InlineData("15", "E1 2EN", "400", "2", "1", HttpStatusCode.OK)]
-    public void
-        Service_Search_Metrics_Endpoint_Returns_A_200_For_Subsequent_Postcode_Search_As_A_Connect_User_After_the_Service_Search_API_Responds_With_A_Non_200_Response_Code(
-            string radius, string postcode, string statusCode, string searchTriggerEventId, string serviceSearchTypeId,
-            HttpStatusCode expectedStatusCode)
-    {
-        this.Given(s =>
-                _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId,
-                    serviceSearchTypeId))
-            .When(s => _steps.WhenISendARequest())
-            .Then(s => _steps.ThenExpectedStatusCodeReturned(expectedStatusCode))
-            .BDDfy();
-    }
-
+    
     //Negative Scenarios
     [Theory]
-    [InlineData("15", "E1 2EN", "200", "3", "1", HttpStatusCode.InternalServerError)]
-    public void ErrorCode_When_Wrong_SearchTriggerEventType_Is_Sent(string radius, string postcode, string statusCode,
+    [InlineData("15", "E1 2EN", "200", "3", "1", HttpStatusCode.InternalServerError)] // Invalid Search Trigger Event Type
+    [InlineData("15", "E1 2EN", "200", "1", "3", HttpStatusCode.InternalServerError)] // Invalid ServiceSearchTypeId
+    public void Service_Search_Metrics_Endpoint_Returns_Status_Code_Internal_Server_Error(string radius, string postcode, string statusCode,
         string searchTriggerEventId, string serviceSearchTypeId, HttpStatusCode expectedStatusCode)
-    {
-        this.Given(s =>
-                _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId,
-                    serviceSearchTypeId))
-            .When(s => _steps.WhenISendARequest())
-            .Then(s => _steps.ThenExpectedStatusCodeReturned(expectedStatusCode))
-            .BDDfy();
-    }
-
-    [Theory]
-    [InlineData("15", "E1 2EN", "200", "1", "3", HttpStatusCode.InternalServerError)]
-    public void ErrorCode_Message_When_Wrong_ServiceSearchTypeId_Is_Posted(string radius, string postcode,
-        string statusCode, string searchTriggerEventId, string serviceSearchTypeId, HttpStatusCode expectedStatusCode)
-    {
-        this.Given(s =>
-                _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId,
-                    serviceSearchTypeId))
-            .When(s => _steps.WhenISendARequest())
-            .Then(s => _steps.ThenExpectedStatusCodeReturned(expectedStatusCode))
-            .BDDfy();
-    }
-
-    [Theory]
-    [InlineData("500", "E1 2EN", "200", "1", "1", HttpStatusCode.BadRequest)]
-    public void ErrorCode_Message_When_Wrong_SearchRadiusMiles_Is_Posted(string radius, string postcode,
-        string statusCode, string searchTriggerEventId, string serviceSearchTypeId, HttpStatusCode expectedStatusCode)
     {
         this.Given(s =>
                 _steps.GivenIHaveASearchServiceRequest(radius, postcode, statusCode, searchTriggerEventId,
